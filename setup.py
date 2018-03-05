75532ad0f40753c68efdc8761aecb3ea5d675e33
Petru Rares Sincraian <psincraian@outlook.com>
2018-03-05 21:02:24 +0100

    fix: add packages

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="commandbus",
-    version="1.0",
-    packages=[],
+    version="1.0.1",
+    packages=find_packages(),
     author="Petru Rares Sincraian",
     author_email="psincraian@gmail.com",
     description="Command Bus Pattern in Python made easy",
