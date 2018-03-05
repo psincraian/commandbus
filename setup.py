009db6f6fdec11ba1b9c68cd085d386dec88eeba
Petru Rares Sincraian <psincraian@outlook.com>
2018-03-05 19:45:08 +0100

    feat: add setup.pu

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="commandbus",
+    version="1.0",
+    packages=[],
+    author="Petru Rares Sincraian",
+    author_email="psincraian@gmail.com",
+    description="Command Bus Pattern in Python made easy",
+    keywords="commandbus command bus python pattern patterns",
+    url="https://github.com/psincraian/commandbus",
+    license="MIT",
+)
